cc73eb35007ac160e3251a2ec6068d4e20190964
artinn <artinn@mail.ru>
2020-03-22 16:31:58 +0300

    max size

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
+    w.setWindowTitle('Label tool')
+    w.showMaximized()
     w.show()
 
     sys.exit(app.exec_())
