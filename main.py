ac5ab479cd3f8ac65e00b08489e986bc7fc8ffb4
artinn <artinn@mail.ru>
2020-03-22 17:03:54 +0300

    experiments with toolbar

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QToolBar, QVBoxLayout
 from PyQt5.QtGui import QIcon
+
 
 if __name__ == '__main__':
 
@@ -13,6 +14,13 @@ if __name__ == '__main__':
     w.setWindowTitle('Label tool')
     w.showMaximized()
     w.setWindowIcon(QIcon('images/icon.png'))
-    w.show()
 
+    left_tool_bar = QToolBar()
+    left_tool_bar.addAction("hi")
+    left_layout = QVBoxLayout()
+
+    left_layout.setMenuBar(left_tool_bar)
+    w.setLayout(left_layout)
+
+    w.show()
     sys.exit(app.exec_())
