fa33cf87221576baf7bc0996d988a6bc89c690ba
darinabzhinaeva <55949653+darinabzhinaeva@users.noreply.github.com>
2020-03-22 17:56:47 +0400

    add icon

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
-
+from PyQt5.QtGui import QIcon
 
 if __name__ == '__main__':
 
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     w = QWidget()
     w.setWindowTitle('Label tool')
     w.showMaximized()
+    w.setWindowIcon(QIcon('images/icon.png'))
     w.show()
 
     sys.exit(app.exec_())
