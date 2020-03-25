7787585b13103104018dcd2dacf125b80333f30a
darinabzhinaeva <55949653+darinabzhinaeva@users.noreply.github.com>
2020-03-25 19:38:21 +0400

    toolbar

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QToolBar, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QAction, qApp, QMainWindow
 from PyQt5.QtGui import QIcon
 
+class TutorialWinow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('LABEL TOOL')
+        self.statusBar().showMessage('This is a status bar')
+
+        self.toolbar = self.addToolBar('Open')
+
+        open_action = QAction(QIcon('images/open.png'), '  &Open', self)
+        #open_action.setShortcut('Win+E')
+        open_action.setStatusTip('Open file')
+        open_action.triggered.connect(qApp.quit)
+
+        self.toolbar.addAction(open_action)
+        self.setGeometry(300, 300, 400, 200)
+
+        
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.setWindowTitle('Label tool')
-    w.showMaximized()
-    w.setWindowIcon(QIcon('images/icon.png'))
-
-    left_tool_bar = QToolBar()
-    left_tool_bar.addAction("hi")
-    left_layout = QVBoxLayout()
-
-    left_layout.setMenuBar(left_tool_bar)
-    w.setLayout(left_layout)
-
-    w.show()
+    tutorial_window = TutorialWinow()
+    tutorial_window.showMaximized()
+    tutorial_window.setWindowIcon(QIcon('images/icon.png'))
     sys.exit(app.exec_())
