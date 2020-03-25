fc4b4dbface6e8508c3519059e61dfdb3b779f37
darinabzhinaeva <55949653+darinabzhinaeva@users.noreply.github.com>
2020-03-25 20:11:28 +0400

    dialog

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QAction, qApp, QMainWindow, QFileDialog, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QAction, qApp, QMainWindow, QFileDialog
 from PyQt5.QtGui import QIcon
 
 class TutorialWinow(QMainWindow):
@@ -19,53 +19,19 @@ class TutorialWinow(QMainWindow):
         open_action = QAction(QIcon('images/open.png'), '  &Open', self)
         #open_action.setShortcut('Win+E')
         open_action.setStatusTip('Open file')
-        open_action.triggered.connect(qApp.quit)
+        #open_action.triggered.connect(qApp.quit)
 
         self.toolbar.addAction(open_action)
         self.setGeometry(300, 300, 400, 200)
 
-class Example(QMainWindow):
+        fname = QFileDialog.getOpenFileName(self, 'Open file',
+                                            'c:\\', "Image files (*.jpg *.gif)")
 
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-
-    def initUI(self):
-
-        self.textEdit = QTextEdit()
-        self.setCentralWidget(self.textEdit)
-        self.statusBar()
-
-        openFile = QAction(QIcon('open.png'), 'Open', self)
-        openFile.setShortcut('Ctrl+O')
-        openFile.setStatusTip('Open new File')
-        openFile.triggered.connect(self.showDialog)
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(openFile)
-
-        self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('File dialog')
-        self.show()
-
-
-    def showDialog(self):
-
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
-
-        f = open(fname, 'r')
-
-        with f:
-            data = f.read()
-            self.textEdit.setText(data)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     tutorial_window = TutorialWinow()
     tutorial_window.showMaximized()
-    ex = Example()
+
     tutorial_window.setWindowIcon(QIcon('images/icon.png'))
     sys.exit(app.exec_())
